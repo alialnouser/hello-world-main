1a1029ecd6c190f33a9a08b9b21b5113ac2aabfd
alialnouser <ali.alnouser@stmartin.edu>
2020-05-10 21:12:15 -0700

    added local subsection

--- my_topics/src/topic_publisher.py
+++ my_topics/src/topic_publisher.py
@@ -1,21 +1,26 @@
 #!/usr/bin/env python
-import rospy
-from std_msgs.msg import Int32 # standard int
 
-# Setup: initialize node, register topic, set rate
-rospy.init_node( # initialize node
-  'topic_publisher' # node default name
-)
-pub = rospy.Publisher( # register topic w/roscore
-  'counter', # topic name
+# the first line will ensure the interpreter used is the first one
+# in your enviromnet's $PATH
+# all python files start with this line, like the 1st line.
+import rospy
+# importing the rospy, that is a python library for ROS
+from std_msgs.msg import Int32 # importing the standard IntInt32 
+#from std_msgs.msg 
+
+# setup: intialzing node with rospy, setting default name
+rospy.init_node('topic_publisher')
+pub = rospy.Publisher('counter' # register w/roscore, # topic name
   Int32, # topic type
   queue_size=5 # queue size
 )
+# start loop
 rate = rospy.Rate(2) # adaptive rate in Hz
 
 # Loop: publish, count, sleep
 count = 0
-while not rospy.is_shutdown(): # until ctrl-c
+while not rospy.is_shutdown(): # shut down by ctrl-c
     pub.publish(count) # publish count
     count += 1 # increment
     rate.sleep() # set by rospy.Rate above
+    # end loop
